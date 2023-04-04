87e4d847fef63df783133542aaac3deda4dce26c
Guilherme Wanderley Espinola <guilherme.espinola@invillia.com>
2023-04-04 18:22:16 -0300

    verificacao de cpf feita

--- secao-6/exercicios/validacao-cpf.js
+++ secao-6/exercicios/validacao-cpf.js
@@ -8,5 +8,28 @@
 
     mesma coisa para o segundo dígito verificador, mas dessa vez iniciando de 11 até 2, e incluindo o 5 ao final.
 
-
 */
+
+function validaCPF(cpf) {
+    let cpfLimpo = cpf.replace(/\D+/g, '');
+    const cpfArray = Array.from(cpfLimpo);
+    const ultimoDigito = cpfArray.pop();
+    const penultimoDigito = cpfArray.pop();
+    let multiplicador = 10;
+
+    if (!verificaDigito(cpfArray, penultimoDigito, multiplicador)) return false; 
+    
+    cpfArray.push(penultimoDigito);
+    multiplicador = 11;
+    return verificaDigito(cpfArray, ultimoDigito, multiplicador);
+
+    function verificaDigito(cpf, digito, multiplicador) {
+        let total = cpf.reduce(function (contador, atual) {
+            contador += (multiplicador * atual);
+            multiplicador--;
+            return contador;
+        }, 0);
+        return Number(digito) === (11 - (total % 11)) ? true : false;
+    }
+}
+console.log(validaCPF("450.953.790-58"));
