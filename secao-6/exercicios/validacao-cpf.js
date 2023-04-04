6f114ab6e60156ead996ee0aef7704b8e1106092
Guilherme Wanderley Espinola <gwanderleyespinola@gmail.com>
2023-04-04 16:16:57 -0300

    validacao cpf

---
+++ secao-6/exercicios/validacao-cpf.js
@@ -0,0 +1,12 @@
+// 705.484.450-52
+/* 
+    7x  0x 5x 4x 8x 4x 4x 5x 0x
+    10  9  8  7  6  5  4  3  2 = 237
+
+    11 - (total % 11) = 5 (primeiro dígito verificador)
+    Se o número for maior que 9, consideramos 0
+
+    mesma coisa para o segundo dígito verificador, mas dessa vez iniciando de 11 até 2, e incluindo o 5 ao final.
+
+
+*/
